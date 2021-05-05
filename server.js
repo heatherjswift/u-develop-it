c17dcf79b0ec96374feb3f5c4a5ff7f34b430e4d
Hede <Heatherj.swift@gmail.com>
2021-05-04 22:25:34 -0400

    build the database calls

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+const mysql = require('mysql2');
+
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Omnihihi1',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found) this needs to go last of the app.use routes
 app.use((req, res) => {
